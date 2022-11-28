a3fd64b0f0dcd641bd4efecc1a7cae8c40071ff4
vincealwyn <vincealwynsibay1@gmail.com>
2022-11-28 15:52:27 +0800

    Setup server skeleton

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("tiny"));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/ping", (_req, res) => {
+	res.json("pong");
+});
+
+app.listen(PORT, () => {
+	console.log(`[server] Server running on port http://localhost:${PORT}/`);
+});
